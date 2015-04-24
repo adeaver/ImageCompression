b07bdeee1b71c0a973ef636b89cba3f5799209b2
adeaver <deaver.andrew@gmail.com>
2015-04-24 10:45:05 -0400

    Removed useless functionsgit add --all! :DDD

--- compress.py
+++ compress.py
@@ -21,44 +21,7 @@ def getGrayscaleValues(img):
 
     return grayscaleValues
 
-def getSamples(step, gValues):
-    return [gValues[index] for index in range(0, len(gValues), step)]
-
-def getXValues(step, gValues):
-    return [index for index in range(0, len(gValues), step)]
-
-def getFreq(n, N, k):
-    freqList = []
-
-    for i in range(n, N):
-        freqList = cos((pi * i * k)/N)
-
-    return freqList
-
 file_name = "file.png"
 
 img = Image.open(file_name)
 gValues = getGrayscaleValues(img)
-
-N = 3000
-
-step = len(gValues)/N
-
-sampled = getSamples(step, gValues)
-xVals = getXValues(step, gValues)
-
-plt.plot(xVals, sampled)
-#plt.plot(gValues)
-plt.ylabel("Grayscale Values")
-plt.xlabel("Time")
-plt.show()
-
-# allFreqs = []
-
-# for x in range(0, N):
-#     allFreqs.append(getFreq(1, N, x))
-
-# freq_matrix = matrix(allFreqs)
-# time_matrix = matrix(sampled[:N]).transpose()
-# print freq_matrix.shape
-# print time_matrix.shape
