2fc07113cfd78fa817ec46d494f1cb61d2b500cc
adeaver <deaver.andrew@gmail.com>
2015-04-29 12:10:37 -0400

    Revamped compression algorithm and added data unpacking

---
+++ compressdct.py
@@ -0,0 +1,134 @@
+from numpy import matrix, array, dot
+from math import sqrt, cos, pi
+from PIL import Image
+from pickle import dump
+
+def getGrayscaleValues(img):
+    width = img.size[0]
+    height = img.size[0]
+
+    grayscaleValues = []
+
+    # im = Image.new("RGB", (width, height))
+    # pixels = im.load()
+
+    for y in range(height):
+        grayScaleRow = []
+        for x in range(width):
+            rgb = img.getpixel((x, y))
+            gray = (rgb[0] * .299 + rgb[1] * .587 + rgb[2] * .114)-127
+            grayScaleRow.append(int(gray))
+            # pixels[x, y] = (int(gray), int(gray), int(gray))
+        grayscaleValues.append(grayScaleRow)
+
+    # im.show()
+
+    return grayscaleValues
+
+def quality(quality):
+    if(quality >= 50):
+        quality = (100-quality)/50.0
+    else:
+        quality = 50.0/quality
+
+    q_matrix = matrix([[16, 11, 10, 16, 24, 40, 51, 61],
+                [12, 12, 14, 19, 26, 58, 60, 55],
+                [14, 13, 16, 24, 40, 57, 69, 56],
+                [14, 17, 22, 29, 51, 87, 80, 62],
+                [18, 22, 37, 56, 68, 109, 103, 77],
+                [24, 35, 55, 64, 81, 104, 113, 92],
+                [49, 64, 78, 87, 103, 121, 120, 101],
+                [72, 92, 95, 98, 112, 100, 103, 99]])
+
+    return q_matrix * quality
+
+
+def quantize(gValues):
+    mat = []
+    step = 8
+    for y_step in range(0, len(gValues)-(len(gValues)%8), step):
+        row = []
+        for x_step in range(0, len(gValues)-(len(gValues[0])%8), step):
+            mini_mat = []
+            for y in range(y_step, y_step+step):
+                mini_row = []
+                for x in range(x_step, x_step+step):
+                    mini_row.append(gValues[y][x])
+                mini_mat.append(mini_row)
+            row = mini_mat
+            # row.append()
+        mat.append(row)
+    return mat
+
+def quality_divide(quality, dct_matrix):
+    new_matrix = []
+    for num_row in range(0, 8):
+        row_divide = list(array(quality[num_row]).reshape(-1))
+        row_dct = list(array(dct_matrix[num_row]).reshape(-1))
+
+        row_final = []
+
+        for element in range(0, 8):
+            row_final.append(round(row_dct[element]/row_divide[element]))
+
+        new_matrix.append(row_final)
+
+    return matrix(new_matrix)
+
+def performDCT(vals, dct):
+    return dot(dot(dct, vals), dct.transpose())
+
+def calcDCT():
+    matrix = []
+    for y in range(0, 8):
+        row = []
+        for x in range(0, 8):
+            if(y == 0):
+                row.append(1/sqrt(8))
+            else:
+                row.append(sqrt(2/8.0) * cos(((2*x+1)*y * pi)/16.0))
+        matrix.append(row)
+
+    return matrix
+
+def intoList(mat):
+    final_matrix = []
+    for index in range(8):
+        row = list(array(mat[index]).reshape(-1))
+        final_matrix.append(row)
+    return final_matrix
+
+def compress(quantized):
+    dct = matrix(calcDCT())
+    quality_matrix = quality(50)
+
+    allSquares = []
+
+    for index in range(len(quantized)):
+        quantize_matrix = matrix(quantized[index])
+        dct_perform = performDCT(quantize_matrix, dct)
+        final_dct = quality_divide(quality_matrix, dct_perform)
+        square = intoList(final_dct)
+        allSquares.append(bytearray(str(square)))
+
+    dump(allSquares, open("compressed.jvad", "wb"))
+
+
+file_name = "xxxrobbiexxx.jpg"
+
+img = Image.open(file_name)
+# img.show()
+
+gValues = getGrayscaleValues(img)
+quantized = quantize(gValues)
+
+verify = []
+for list1 in quantized[0]:
+    row = []
+    for num in list1:
+        row.append(num+127)
+    verify.append(row)
+
+print verify
+
+compress(quantized)
--- reloadimage.py
+++ reloadimage.py
@@ -1,22 +1,84 @@
 from pickle import load
 from math import cos, pi
+from compressdct import quality, calcDCT, intoList
+from PIL import Image
+from numpy import dot, matrix, array
+import re
 
-def getValues(gValues):
-    C_matrix = []
-    for l in range(len(gValues)):
-        C_row = []
-        for k in range(len(gValues[0])):
-            I_sum = 0
-            for m in range(len(gValues)):
-                for n in range(len(gValues[0])):
-                    I_sum += gValues[m][n] * cos((pi * (k * n + l * m))/len(gValues))
-            C_row.append(I_sum)
-        C_matrix.append(C_row)
-    return C_matrix
+def reverse_quality(quality, dct_matrix):
+    new_matrix = []
+    for num_row in range(0, 8):
+        row_divide = list(array(quality[num_row]).reshape(-1))
+        row_dct = list(array(dct_matrix[num_row]).reshape(-1))
 
-data = load(open("compress.jvad", "rb"))
+        row_final = []
 
-print getValues(data[0])
+        for element in range(0, 8):
+            row_final.append(round(row_dct[element] * row_divide[element]))
 
-# for line in data[0]:
-#     print line
+        new_matrix.append(row_final)
+
+    return matrix(new_matrix)
+
+def performIDCT(dct, img):
+    return dot(dot(dct.transpose(), img), dct) + 128
+
+def roundElements(mat):
+    rounded = []
+
+    for base in range(len(mat)):
+        row = []
+        for x in range(len(mat[base])):
+            row.append(int(mat[base][x]))
+        rounded.append(row)
+
+    return rounded
+
+def stringToList(string):
+    mat = []
+    initFormat = re.sub("\[", "", string[1:len(string)-1])
+    listFormat = initFormat.split("]")
+    
+    for newList in listFormat:
+        numList = newList.split(", ")
+        row = []
+        for num in numList:
+            if(len(num)>0):
+                row.append(float(num))
+
+        if(len(row) > 0):
+            mat.append(row)
+
+    return mat
+
+def loadAndUnpack():
+    data_packed = load(open("compressed.jvad", "rb"))
+
+    data = []
+    mat = []
+
+    for index in range(len(data_packed)):
+        line = data_packed[index]
+        mat = stringToList(line.decode('utf-8'))
+        data.append(mat)
+
+    return data
+
+
+def uncompress(data):
+    quality_matrix = quality(50)
+    dct = matrix(calcDCT())
+
+    final_data = []
+
+    for index in range(len(data)):
+        data_mat = matrix(data[index])
+        reved = reverse_quality(quality_matrix, data_mat)
+        newimg = performIDCT(dct, reved)
+        imglist = roundElements(intoList(newimg))
+        final_data.append(imglist)
+
+        if(index == 0):
+            print imglist
+
+uncompress(loadAndUnpack())
